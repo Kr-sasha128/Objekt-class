d1d2eec5eeef9aac568d85e29a5febd84c4da376
Alexander-PC <br128128@g,ail.com>
2024-12-06 17:42:12 +0300

    Домашняя работа "Объекты и Классы"

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        Author gogol = new Author("Николай", " Гоголь ");
+        Book deadSouls = new Book("Мёртвые души ", gogol, 1842);
+        deadSouls.setYearOfTheBook(1845);
+        System.out.println(deadSouls.getBookInfo());
+
+        Author annaAkhmatova = new Author("Анна ", "Ахматова ");
+        Book grayKing = new Book("Сероглазый король ", annaAkhmatova, 1912);
+        System.out.println(grayKing.getBookInfo());
+    }
+}
